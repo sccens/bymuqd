0b34c5013e4a0fb1056484a313df5e5cd460abd9
sccens <80002252+sccens@users.noreply.github.com>
2023-12-04 21:45:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+#抓包自己账号TOEKN，'LATITUD''LONGITUDE'填写自己实习地址的经纬度
+
 import requests
+import uuid
 headers = {
 'Token': 'TOKEN',
 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
+
 }
 data = {
     'lat': 'LATITUDE',
     'lng': 'LONGITUDE',
-    'phoneUuid': 'UUID',
+    'phoneUuid': uuid.uuid4().hex,
     'taskId': '89',
     'type': '1',
 }
